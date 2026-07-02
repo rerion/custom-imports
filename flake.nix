88285e218e4e23efcb8eb6171c7fb9a653a4c681
rerion <phearion@gmail.com>
2026-07-02 18:04:22 +0200

    chore: environment setup

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Local Node.js development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs_22
+            pnpm
+            typescript
+            typescript-language-server
+          ];
+
+          shellHook = ''
+            echo "custom-imports dev shell (Node $(node --version))"
+          '';
+        };
+      });
+}
